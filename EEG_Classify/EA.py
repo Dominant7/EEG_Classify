923723b611230de428dc047b79562752692fa1c2
何佳辰 <1518179954@qq.com>
2024-06-14 18:02:19 +0800

    添加项目文件。

---
+++ EEG_Classify/EA.py
@@ -0,0 +1,24 @@
+﻿import numpy
+from scipy.linalg import sqrtm, inv
+
+def EA(rawArray):
+    '''
+    输入要处理的numpy array(格式为[trails * 通道* 采样点])
+    返回进行欧拉对齐后的numpy array
+    '''
+    transMatrixList = []
+    covarianceMatrixList = []
+    for trail in rawArray: # 遍历trail
+        # covarianceMatrixList = []
+        # for trail in rawArray:
+        #     covarianceMatrix = numpy.cov(trail[i])
+        #     covarianceMatrixList.append(covarianceMatrix)
+        covarianceMatrix = numpy.cov(trail, rowvar=True)
+        covarianceMatrixList.append(covarianceMatrix)
+    covarianceMatrixMean = numpy.mean(covarianceMatrixList, axis=0)
+    transMatrix = inv(sqrtm(covarianceMatrixMean))
+    processedArray = numpy.empty_like(rawArray)
+    for i in range(processedArray.shape[0]): # 遍历trail
+        processedArray[i] = transMatrix @ rawArray[i]
+            
+    return processedArray
---
+++ EEG_Classify/EEG_Classify.py
@@ -0,0 +1,33 @@
+﻿import numpy
+import EA
+
+# 读取训练数据
+trainDataDictRaw = {} # 未经对齐的训练集数据
+trainDataDictEA = {} # 经过EA对齐的训练集数据
+dataList = []
+fileNames = ['s1.npz', 's2.npz', 's3.npz', 's4.npz']
+for fileName in fileNames:
+    with numpy.load('./Data\\train\\' + fileName) as data:
+        data_X = data['X']
+        data_y = data['y']
+        # print(data_X.shape)
+        # print(data_y.shape)
+        dataList.append((data_X, data_y))
+# print(trainDataDict)
+trainDataDictRaw.update(dict(zip(fileNames, dataList)))
+
+
+# Xmean = []
+# for key in trainDataDictRaw:
+#     Xmean.append(numpy.mean(numpy.mean(trainDataDictRaw[key][0], axis=2), axis=0))
+# print(Xmean)
+
+# 进行欧拉对齐
+dataList = []
+for key in trainDataDictRaw:
+    data_X, data_y = trainDataDictRaw[key]
+    data_X = EA.EA(data_X)
+    dataList.append((data_X, data_y))
+trainDataDictEA.update(dict(zip(fileNames, dataList)))
+
+pass
